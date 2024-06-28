9c197daaec7094aa3d07e2af8a9dee4a26381c33
Mikemupararano <kudath@yahoo.co.uk>
2024-06-28 15:34:46 +0100

    Added a tab to allow a user to remove a question or add one. Also, I added a user to select the type of input they want.

--- src/components/SurveyForm.jsx
+++ src/components/SurveyForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form, Button, Col, Row } from 'react-bootstrap';
+import { Form, Button, Col, Row, Container } from 'react-bootstrap';
 import axios from 'axios';
 
 const SurveyForm = () => {
@@ -41,6 +41,25 @@ const SurveyForm = () => {
     setProductQuestions(updatedProducts);
   };
 
+  const addQuestion = () => {
+    const newQuestion = { id: questions.length + 1, type: 'text', label: '', value: '' };
+    setQuestions([...questions, newQuestion]);
+  };
+
+  const removeQuestion = (id) => {
+    setQuestions(questions.filter((question) => question.id !== id));
+  };
+
+  const changeQuestionType = (id, type) => {
+    const updatedQuestions = questions.map((question) => {
+      if (question.id === id) {
+        return { ...question, type: type, value: '', options: type === 'select' ? ['Option 1', 'Option 2'] : undefined };
+      }
+      return question;
+    });
+    setQuestions(updatedQuestions);
+  };
+
   const addProduct = () => {
     const newProduct = { id: productQuestions.length + 1, name: '', description: '', materials: '', recycled: '', origin: '' };
     setProductQuestions([...productQuestions, newProduct]);
@@ -61,71 +80,136 @@ const SurveyForm = () => {
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      {questions.map((question) => (
-        <Form.Group key={question.id}>
-          <Form.Label>{question.label}</Form.Label>
-          {question.type === 'textarea' ? (
-            <Form.Control as="textarea" rows={3} value={question.value} onChange={(e) => handleInputChange(e, question.id)} />
-          ) : question.type === 'select' ? (
-            <Form.Control as="select" value={question.value} onChange={(e) => handleInputChange(e, question.id)}>
-              <option value="">Select...</option>
-              {question.options.map((option, index) => (
-                <option key={index} value={option}>
-                  {option}
-                </option>
-              ))}
-            </Form.Control>
-          ) : (
-            <Form.Control type={question.type} value={question.value} onChange={(e) => handleInputChange(e, question.id)} />
-          )}
-        </Form.Group>
-      ))}
-      <h4>Product Information</h4>
-      {productQuestions.map((product) => (
-        <div key={product.id}>
-          <Row>
-            <Col>
-              <Form.Group>
-                <Form.Label>Product Name</Form.Label>
-                <Form.Control type="text" value={product.name} onChange={(e) => handleProductChange(e, product.id, 'name')} />
-              </Form.Group>
-            </Col>
-            <Col>
-              <Form.Group>
-                <Form.Label>Description</Form.Label>
-                <Form.Control type="text" value={product.description} onChange={(e) => handleProductChange(e, product.id, 'description')} />
-              </Form.Group>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Form.Group>
-                <Form.Label>Material Composition</Form.Label>
-                <Form.Control type="text" value={product.materials} onChange={(e) => handleProductChange(e, product.id, 'materials')} />
-              </Form.Group>
-            </Col>
-            <Col>
-              <Form.Group>
-                <Form.Label>Percentage of Recycled Materials Used</Form.Label>
-                <Form.Control type="number" value={product.recycled} onChange={(e) => handleProductChange(e, product.id, 'recycled')} />
-              </Form.Group>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Form.Group>
-                <Form.Label>Country of Origin</Form.Label>
-                <Form.Control type="text" value={product.origin} onChange={(e) => handleProductChange(e, product.id, 'origin')} />
-              </Form.Group>
-            </Col>
-          </Row>
-        </div>
-      ))}
-      <Button variant="secondary" onClick={addProduct}>Add More Products</Button>
-      <Button variant="primary" type="submit">Submit</Button>
-    </Form>
+    <Container>
+      <Form onSubmit={handleSubmit}>
+        {questions.map((question) => (
+          <Form.Group key={question.id}>
+            <Row className="align-items-center mb-3">
+              <Col md={4}>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter question label"
+                  value={question.label}
+                  onChange={(e) => handleInputChange(e, question.id)}
+                />
+              </Col>
+              <Col md={4}>
+                <Form.Control
+                  as="select"
+                  value={question.type}
+                  onChange={(e) => changeQuestionType(question.id, e.target.value)}
+                >
+                  <option value="text">Text</option>
+                  <option value="textarea">Textarea</option>
+                  <option value="number">Number</option>
+                  <option value="email">Email</option>
+                  <option value="select">Select</option>
+                </Form.Control>
+              </Col>
+              <Col md={4}>
+                <Button variant="danger" onClick={() => removeQuestion(question.id)}>Remove</Button>
+              </Col>
+            </Row>
+            {question.type === 'textarea' ? (
+              <Form.Control
+                as="textarea"
+                rows={3}
+                placeholder={question.label}
+                value={question.value}
+                onChange={(e) => handleInputChange(e, question.id)}
+              />
+            ) : question.type === 'select' ? (
+              <Form.Control
+                as="select"
+                value={question.value}
+                onChange={(e) => handleInputChange(e, question.id)}
+              >
+                <option value="">Select...</option>
+                {question.options && question.options.map((option, index) => (
+                  <option key={index} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </Form.Control>
+            ) : (
+              <Form.Control
+                type={question.type}
+                placeholder={question.label}
+                value={question.value}
+                onChange={(e) => handleInputChange(e, question.id)}
+              />
+            )}
+          </Form.Group>
+        ))}
+        <Button variant="secondary" onClick={addQuestion}>Add Question</Button>
+        <h4 className="mt-4">Product Information</h4>
+        {productQuestions.map((product) => (
+          <div key={product.id}>
+            <Row>
+              <Col>
+                <Form.Group>
+                  <Form.Label>Product Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={product.name}
+                    onChange={(e) => handleProductChange(e, product.id, 'name')}
+                  />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group>
+                  <Form.Label>Description</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={product.description}
+                    onChange={(e) => handleProductChange(e, product.id, 'description')}
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Form.Group>
+                  <Form.Label>Material Composition</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={product.materials}
+                    onChange={(e) => handleProductChange(e, product.id, 'materials')}
+                  />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group>
+                  <Form.Label>Percentage of Recycled Materials Used</Form.Label>
+                  <Form.Control
+                    type="number"
+                    value={product.recycled}
+                    onChange={(e) => handleProductChange(e, product.id, 'recycled')}
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Form.Group>
+                  <Form.Label>Country of Origin</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={product.origin}
+                    onChange={(e) => handleProductChange(e, product.id, 'origin')}
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+          </div>
+        ))}
+        <Button variant="secondary" onClick={addProduct}>Add More Products</Button>
+        <Button variant="primary" type="submit">Submit</Button>
+      </Form>
+    </Container>
   );
 };
 
 export default SurveyForm;
+
+
